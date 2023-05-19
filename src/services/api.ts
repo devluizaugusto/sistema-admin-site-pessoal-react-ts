ba9b1a5018a3a06fae129a102a4872a9e0fd1e61
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-05-19 13:58:52 -0300

    Atualizado arquivo api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: apiUrl,
+    baseURL: "https://json-server-node-vlgw.onrender.com",
 });
 
 export default api;
